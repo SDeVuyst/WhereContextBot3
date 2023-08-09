83040602f853aee3b09711eefaf51d856bbc3cec
De Vuyst Silas <silas.devuyst@ugent.be>
2023-08-09 11:18:10 +0200

    work on cost_nword

--- bot.py
+++ bot.py
@@ -339,7 +339,7 @@ async def on_voice_state_update(member, before, after) -> None:
 
 
 @bot.event
-async def on_app_command_error(context: Context, error) -> None:
+async def on_command_error(context: Context, error) -> None:
     """
     The code in this event is executed every time a command catches an error.
 
--- cogs/reacties.py
+++ cogs/reacties.py
@@ -7,33 +7,6 @@ import discord
 from helpers import checks, db_manager 
 from exceptions import MissingNwords
 
-def cost_nword(cost):
-
-    async def predicate(ctx: commands.Context):
-        
-        nword_count = await db_manager.get_nword_count(ctx.author.id)
-        print(cost, nword_count)
-
-        # Geen berichten
-        if len(nword_count) == 0 or int(nword_count[0][0]) == 0:
-            exact_count = 0
-        
-        # error
-        elif nword_count[0] == -1:
-            raise Exception("Could not fetch nword count!")
-        
-        else:
-            exact_count = int(nword_count[0][0])
-
-        # user heeft niet genoeg nwords
-        if exact_count < cost:
-            raise MissingNwords(exact_count, cost)
-        
-        # update nword count
-        return await db_manager.set_nword_count(ctx.author.id, exact_count-cost)
-
-    return commands.check(predicate)
-
 
 # Here we name the cog and create a new class for the cog.
 class Reacties(commands.Cog, name="reacties"):
@@ -43,7 +16,6 @@ class Reacties(commands.Cog, name="reacties"):
 
     @app_commands.command(name="wholesquadlaughing", description="damn bro you got the whole squad laughing", extras={'cog': 'reacties'})
     @checks.not_blacklisted()
-    @cost_nword(5)
     async def wholesquadlaughing(self, interaction):
         """Sends the 'whole squad laughing' reaction
 
