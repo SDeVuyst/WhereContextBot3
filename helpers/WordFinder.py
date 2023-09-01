b85cd9b244c0a3521d5ea06fc9abebb434b4bf86
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2023-09-01 20:10:32 +0200

    bugfix

--- helpers/WordFinder.py
+++ helpers/WordFinder.py
@@ -71,7 +71,17 @@ class WordFinder:
             f"<@{message.author.id}> it's on sight now",
         ]
 
+        # gif via link
         if ".gif" in message.content:
             # gif found
             await message.channel.send(random.choice(responses))
             await message.delete()
+            return
+        
+        # gif via attachments
+        if len(message.attachments) > 0:
+            for attachment in message.attachments:
+                if ".gif" in attachment.filename.lower():
+                    await message.channel.send(random.choice(responses))
+                    await message.delete()
+                    return
