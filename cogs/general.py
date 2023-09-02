768ee85a306d64266592de1de8c42a5632455ff7
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2023-09-02 17:51:07 +0200

    finished impersonate command

--- cogs/general.py
+++ cogs/general.py
@@ -480,7 +480,7 @@ class General(commands.Cog, name="general"):
     @app_commands.command(name="impersonate", description="Send a message diguised as a user (10🪙)", extras={'cog': 'general'})
     @checks.not_blacklisted()
     @checks.not_in_dm()
-    # @checks.cost_nword(10)
+    @checks.cost_nword(10)
     @app_commands.describe(user="Who to impersonate")
     @app_commands.describe(message="What to say")
     async def impersonate(self, interaction, user: discord.User, message: str) -> None:
@@ -492,12 +492,10 @@ class General(commands.Cog, name="general"):
             messate (str): what to say
         """
 
-        await interaction.response.defer()
-
         # creeer webhook en stuur via die webhook impersonatie
         webhook = await interaction.channel.create_webhook(name=user.name)
         await webhook.send(
-            str(message), username=user.name, avatar_url=user.display_avatar.url, silent=True
+            str(message), username=user.name, avatar_url=user.display_avatar.url
         )
 
         await webhook.delete()
@@ -508,7 +506,7 @@ class General(commands.Cog, name="general"):
             color=self.bot.succesColor
         )
         # stuur het antwoord
-        await interaction.followup.send(embed=embed, ephemeral=True)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 async def setup(bot):
