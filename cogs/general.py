ac915b33a2aca994561cfb97c7609396fd7557a1
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2023-09-09 20:59:12 +0200

    added blank line after options & fixed url

--- cogs/general.py
+++ cogs/general.py
@@ -586,9 +586,9 @@ class PollMenuBuilder(discord.ui.View):
 
         
         # add options to embed
-        opts = '\u200b' +'\n'.join(f'**{self.reactions[index]}: {val}**' for index, val in enumerate(self.options))
+        opts = '\n'.join(f'**{self.reactions[index]}: {val}**' for index, val in enumerate(self.options))
         self.embed.remove_field(index=1)
-        self.embed.add_field(name="**📋 Options**", value=opts, inline=False)
+        self.embed.add_field(name="**📋 Options**", value=opts + "\n\u200b", inline=False)
         
         
         for b in self.children:
@@ -662,7 +662,7 @@ class PollMenuBuilder(discord.ui.View):
         self.embed.title = f'***{self.title}***'
         # todo
         self.embed.set_thumbnail(
-            url=f"https://quickchart.io/chart?c={{type:'pie',data:{{ datasets:[{{data:{','.join(['1' for _ in range(len(self.options))])},}}]}}}} "
+            url=f"https://quickchart.io/chart?c={{type:'pie',data:{{datasets:[{{data:[{','.join(['1' for _ in range(len(self.options))])}]}}]}}}}"
         )         
 
         # edit original message
