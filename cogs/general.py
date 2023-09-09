fb014a403d6d442e1af4651c619324241aaa0382
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2023-09-09 21:06:25 +0200

    added options to pie chart

--- cogs/general.py
+++ cogs/general.py
@@ -662,7 +662,7 @@ class PollMenuBuilder(discord.ui.View):
         self.embed.title = f'***{self.title}***'
         # todo
         self.embed.set_thumbnail(
-            url=f"https://quickchart.io/chart?c={{type:'pie',data:{{datasets:[{{data:[{','.join(['1' for _ in range(len(self.options))])}]}}]}}}}"
+            url=f"https://quickchart.io/chart?c={{type:'pie',data:{{datasets:[{{data:[{','.join(['1' for _ in range(len(self.options))])}]}}],labels:[{','.join(self.options)}]}}}}"
         )         
 
         # edit original message
