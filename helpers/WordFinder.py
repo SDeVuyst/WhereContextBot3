c4b4f5dea348bb817b6688646a749ae735911c07
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2023-10-05 20:31:11 +0200

    Update WordFinder.py

--- helpers/WordFinder.py
+++ helpers/WordFinder.py
@@ -89,7 +89,7 @@ class WordFinder:
     async def check_gf(self, bot, message):
         content = message.content.replace("\n", " ").lower()
         triggers = ["danae", "danea", "danaë"]
-        responses = ["<@222415043550117888> pedo ass mf", '<@222415043550117888> pre-ordered :sob:', "<@222415043550117888> bro going to jail :sob::sob:", "<@222415043550117888> early investor :skull:", "https://i.imgur.com/Qtjijog.gif", "https://imgur.com/a/vybyj9s"]
+        responses = ["<@222415043550117888> pedo ass mf", '<@222415043550117888> pre-ordered :sob:', "<@222415043550117888> bro going to jail :sob::sob:", "<@222415043550117888> early investor :skull:", "https://i.imgur.com/Qtjijog.gif", "https://imgur.com/a/vybyj9s", "https://imgur.com/a/WAGkAUF"]
 
         # check for a trigger in the message
         if any(trigger in content for trigger in triggers):
