6c940b83b2945b281512f4ab01240f6ec031f19e
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2024-01-14 22:51:15 +0100

    added poses and yachja podium

--- cogs/admin.py
+++ cogs/admin.py
@@ -596,7 +596,7 @@ class Admin(commands.Cog, name="admin"):
         # show podiums if user has one
         builder = PodiumBuilder.PodiumBuilder(self.bot)
         if builder.userHasPodium(user.id):
-            file = await builder.getAllPodiumsImage([user.id, user.id, user.id], padding=100)
+            file = await builder.getAllPodiumsImage([user.id, user.id, user.id], padding=100, add_characters=False)
             embed.set_image(url="attachment://podium.png")
         
             return await interaction.followup.send(embed=embed, files=[file], view=ConfigureView(self.bot, embed, user.id))
@@ -751,7 +751,7 @@ class RolesSelectView(discord.ui.View):
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red, row=3, disabled=False, emoji="✖️")
     async def close_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
         embed = discord.Embed(
-            title=f"Canceled!",
+            title=f"Cancelled!",
             color=self.bot.defaultColor,
         )
         await interaction.response.edit_message(embed=embed, view=None, delete_after=10)
--- helpers/PodiumBuilder.py
+++ helpers/PodiumBuilder.py
@@ -4,32 +4,100 @@ import discord
 import io
 import requests
 import random
-import os
 
-from helpers import db_manager
+# from helpers import db_manager
 
 class PodiumBuilder:
     def __init__(self, bot) -> None:
         self.bot = bot
 
-        self.definedPodiums = {
-            "334371900170043402": "arion/ArionPodium",
-            "273503117348306944": "arno/ArnoPodium",
-            "222415043550117888": "ba/BaPodium",
-            "559715606014984195": "gible/GiblePodium",
-            "494508091283603462": "jacko/JackoPodium",
-            "339820557086228490": "jakob/JakobPodium",
-            "527916521754722315": "leander/LeanderPodium",
-            "512256261459542019": "meng/MengPodium",
-            "464400950702899211": "pingy/PingyPodium",
-            "462932133170774036": "silas/SolosPodium",
-            "453136562885099531": "wouter/WouterPodium",
-            "": "yachja/YachjaPodium", # TODO
-            "756527409876041859": "zeb/ZebPodium",
+        self.definedArt = {
+            "334371900170043402": {
+                "podiumLocation": "arion/ArionPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "273503117348306944": {
+                "podiumLocation": "arno/ArnoPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "222415043550117888": {
+                "podiumLocation": "ba/BaPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "559715606014984195": {
+                "podiumLocation": "gible/GiblePodium",
+                "shinyPodiumLocation": "gible/ShinyGiblePodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "494508091283603462": {
+                "podiumLocation": "jacko/JackoPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "339820557086228490": {
+                "podiumLocation": "jakob/JakobPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "527916521754722315": {
+                "podiumLocation": "leander/LeanderPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "512256261459542019": {
+                "podiumLocation": "meng/MengPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "464400950702899211": {
+                "podiumLocation": "pingy/PingyPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "462932133170774036": {
+                "podiumLocation": "silas/SolosPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+                "poseOffset": [
+                    (0, 100), (0, 200), (0, 340)
+                ]
+            },
+            "453136562885099531": {
+                "podiumLocation": "wouter/WouterPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
+            "733845345225670686": {
+                "podiumLocation": "yachja/YachjaPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },  
+            "756527409876041859": {
+                "podiumLocation": "zeb/ZebPodium",
+                "badgePasteCoords": [
+                    (255, 1050), (255, 1130), (255, 1320)
+                ],  
+            },
         }
 
     def userHasPodium(self, user_id):
-        return str(user_id) in self.definedPodiums
+        return str(user_id) in self.definedArt
 
     async def getLeaderboard(self, leaderboard, command):
         # self.bot.logger.info(leaderboard)
@@ -106,19 +174,61 @@ class PodiumBuilder:
         return discord.File(buffer, 'leaderboard.png')
     
 
-    def getPodiumImage(self, user_id, place, alwaysSpecial = False):
-        location = self.definedPodiums.get(str(user_id), "default/DefaultPodium")
+    async def getPodiumImage(self, user_id, place, alwaysShiny = False):
+        # get object that contains info about the arts done
+        defined_art = self.definedArt.get(str(user_id), {})
 
-        if location == "gible/GiblePodium" and alwaysSpecial:
-            # 1/20 chance gible podium is shiny
-            location = "gible/ShinyGiblePodium"
-            
-        image = Image.open(f'media/images/{location}{place}.png')
+        # get location of the podium
+        location = defined_art.get("podiumLocation", "default/DefaultPodium")
 
+        # check if podium can be shiny and if it should be
+        if "shinyPodiumLocation" in defined_art and alwaysShiny:
+            location = defined_art.get("shinyPodiumLocation")
+        
+        # load the selected image
+        # image = Image.open(f'media/images/{location}{place}.png')
+
+        image = Image.open(f'C:/Users/Silas/OneDrive/Documenten/GitHub/WhereContextBot3/media/images/{location}{place}.png')
         return image
-
     
-    async def getAllPodiumsImage(self, user_ids, return_file=True, padding=200, color=(44, 45, 47)):
+    async def addCharacterToPodium(self, podiumImage, user_id, poseNumber, place):
+        # get object that contains info about the arts done
+        defined_art = self.definedArt.get(user_id, {})
+
+        # get location of the character
+        customCharacterLocation = defined_art.get("characterLocation", "default/DefaultPose")
+
+        # load the character image
+        characterImage = Image.open(f'C:/Users/Silas/OneDrive/Documenten/GitHub/WhereContextBot3/media/images/{customCharacterLocation}{poseNumber}.png')
+        # characterImage = Image.open(f'media/images/{customCharacterLocation}{poseNumber}.png')
+        # resize it
+        characterImage = resize_image(characterImage, 700)
+
+        # create empty bg to add podium and character to
+        bg = Image.new('RGB', (podiumImage.width, podiumImage.height + characterImage.height), (44, 45, 47))
+        
+        # paste podium
+        bg.paste(podiumImage, (0, characterImage.height), podiumImage)
+
+        # paste character
+        offset = defined_art.get("poseOffset", [(0, 110), (0, 200), (0, 370)])[place-1]
+        bg.paste(
+            characterImage,
+            ((bg.width - characterImage.width)//2 + offset[0], offset[1]), 
+            characterImage
+        )
+
+        # paste badge to fix 3d realness of character standing on podium
+        pasteCoords = defined_art.get("badgePasteCoords", [(255, 1050), (255, 1130), (255, 1320)])
+        #badgeImage = Image.open(f"media/images/Badge{place}.png")
+        badgeImage = Image.open(f"C:/Users/Silas/OneDrive/Documenten/GitHub/WhereContextBot3/media/images/Badge{place}.png")
+        bg.paste(badgeImage, pasteCoords[place-1], badgeImage)
+
+        # bg.show()
+        return bg
+    
+    
+    async def getAllPodiumsImage(self, user_ids, return_file=True, padding=200, color=(44, 45, 47), add_characters=True):
         # create normalised images for every given podium
         if len(user_ids) == 1:
             order = [1]
@@ -127,28 +237,47 @@ class PodiumBuilder:
         else:
             order = [2, 1, 3]
         
+        # 1 in 12 chance that the podiums are shiny (if possible)
+        alwaysShiny = random.randint(1, 12) == 2
+
+        podiums = []
+        # for every available user (top 3)
+        for i, id in enumerate(user_ids):
+            # get podium
+            podiumImage = await self.getPodiumImage(id, order[i], alwaysShiny) 
+            
+            if add_characters: 
+                # add character
+                podiumImage = await self.addCharacterToPodium(podiumImage, str(id), 1, order[i]) # TODO pose picker
+            
+            # fill empty background
+            podiumImage = remove_transparency(podiumImage, color)
+
+            podiums.append(podiumImage)
+
         
-        alwaysSpecial = random.randint(1, 12) == 2
-        podiums = [remove_transparency(self.getPodiumImage(id, order[i], alwaysSpecial), color=color) for i, id in enumerate(user_ids)]
-        
-        # dm gible
-        if alwaysSpecial:
-            GIBLE_ID = int(os.environ.get("GIBLE_ID"))
-            await db_manager.increment_or_add_nword(GIBLE_ID, 500)
+        # dm user if podium is shiny
+        if alwaysShiny:
+            for user_id in user_ids:
+                if "shinyPodiumLocation" in self.definedArt.get(str(user_id), {}):
+                
+                    # await db_manager.increment_or_add_nword(int(user_id), 500)
 
-            embed = discord.Embed(
-                title='🎉 Congratulations!',
-                description=f"A shiny version of gible has been pulled! You have been awarded 500 n-words.",
-                color=self.bot.succesColor,
-            )
+                    embed = discord.Embed(
+                        title='🎉 Congratulations!',
+                        description=f"A shiny version of your podium has been pulled! You have been awarded 500 n-words.",
+                        color=self.bot.succesColor,
+                    )
 
-            gible_user = self.bot.get_user(GIBLE_ID)
-            await gible_user.send(embed=embed)
+                    user = self.bot.get_user(int(user_id))
+                    # TODO uncomment 
+                    # await user.send(embed=embed)
 
 
-        # paste podiums on correct location
+        # paste podiums on correct location # TODO fix yachja overlapping pod
         dst = get_concat_h_multi_blank(podiums, padding, color=color)
 
+        # return as image if necessary
         if not return_file:
             return dst
         
@@ -164,13 +293,14 @@ class PodiumBuilder:
         return discord.File(buffer, 'podium.png')   
     
 
+# concat multiple images
 def get_concat_h_multi_blank(im_list, padding, color=(44, 45, 47)):
     _im = im_list.pop(0)
     for im in im_list:
         _im = get_concat_h_blank(_im, im, padding, color)
     return _im
 
-
+# concat 2 images
 def get_concat_h_blank(im1, im2, padding, color=(44, 45, 47)):
     dst = Image.new('RGB', (im1.width + im2.width + padding, im1.height), color)
     dst.paste(im1, (0, 0))
@@ -199,6 +329,7 @@ def resize_image(im, width, max_height=1000):
     return im.resize((width, hsize), Image.Resampling.LANCZOS)
 
 
+# get number in human format
 def human_format(num):
     num = float('{:.3g}'.format(num))
     magnitude = 0
